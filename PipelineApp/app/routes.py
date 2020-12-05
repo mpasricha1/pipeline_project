e2015fffcdc64a2ed0fdde3215a9c0f064f9f78c
Mark Pasricha <markp862@gmail.com>
2020-12-05 10:25:07 -0500

    Added some placeholders on index.html

--- PipelineApp/app/routes.py
+++ PipelineApp/app/routes.py
@@ -1,12 +1,10 @@
 from flask import render_template, request, redirect, session, url_for
 from flask.json import jsonify
-from sqlalchemy.ext.automap import automap_base 
-from sqlalchemy.ext.declarative import declarative_base 
-from sqlalchemy import create_engine, and_, func
-from sqlalchemy.orm import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from app import app 
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template("base.html")
+	return render_template("index.html")
