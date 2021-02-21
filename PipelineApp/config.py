112c614f4a6ba9ec17a8b8c4aa790e62c8889a59
Mark Pasricha <markp862@gmail.com>
2021-02-20 20:20:45 -0500

    adding to config file

--- PipelineApp/config.py
+++ PipelineApp/config.py
@@ -8,3 +8,4 @@ class Config(object):
 	# SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:postgres@localhost/pipeline_db_dev"
 	SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:tableBlackCream177@threepm-dev1.chj3uciqytlo.us-east-1.rds.amazonaws.com:5432/threepm-dev1"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
